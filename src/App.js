b33e7658ea30f4cdc10512068322ca8e8c0321ab
syed1255 <99645911+syed1255@users.noreply.github.com>
2022-02-14 22:16:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
-// import React from 'react'
 
-// const App = () => {
-//     return (
-//         <div className="todo-app">
-//           <h1>todolist</h1>  
-//         </div>
-//     )
-// }
-
-// export default App
   
 import React from 'react';
 import './App.css';
 import TodoList from './components/TodoList';
-import TodoFrom from './components/TodoForm'
+
 
 function App() {
   return (
